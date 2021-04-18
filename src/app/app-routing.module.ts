2d9617fbe5dd9d1b49bdf60b84a1e310c14f98bd
Ashraf Shaker <sayedashrafshaker@yahoo.com>
2021-04-19 01:29:37 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddCtegoryComponent } from './Category/add-ctegory/add-ctegory.component';
-import { CategoriesListComponent } from './Category/categories-list/categories-list.component';
-import { EditCategoryComponent } from './Category/edit-category/edit-category.component';
+import { AddCtegoryComponent } from '../admin/Category/add-ctegory/add-ctegory.component';
+import { CategoriesListComponent } from '../admin/Category/categories-list/categories-list.component';
+import { EditCategoryComponent } from '../admin/Category/edit-category/edit-category.component';
 
 import { LoginComponent } from './auth/login/login.component';
 import { ProfileComponent } from './auth/profile/profile.component';
@@ -10,6 +10,10 @@ import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   {path:"category/add", component:AddCtegoryComponent},
+  {
+    path: 'category/add',
+    loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule)
+  },
   {path:"categories", component:CategoriesListComponent},
   {path:"categories/edit/:id", component:EditCategoryComponent},
   { path: 'register', component: RegisterComponent },
